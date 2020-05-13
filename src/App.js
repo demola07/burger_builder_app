286880abeee090fa70fa2b91b37bb810e50881d0
demola07 <45898468+demola07@users.noreply.github.com>
2020-05-13 09:56:25 +0100

    Strurcuting Layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
+import Layout from './components/Layout/Layout';
 
-function App() {
-  return <div className='App'></div>;
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Layout>
+          <p>Test</p>
+        </Layout>
+      </div>
+    );
+  }
 }
 
 export default App;
