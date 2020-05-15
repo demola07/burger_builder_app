7b51e8268e7ad1e2c101ac4555def9cf1faa7ca0
demola07 <45898468+demola07@users.noreply.github.com>
2020-05-15 15:28:16 +0100

    fix bug with active lin in toolbar

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -8,9 +8,7 @@ const navigationItems = () => (
     <NavigationItem link='/' active>
       Burger Builder
     </NavigationItem>
-    <NavigationItem link='/' active>
-      Checkout
-    </NavigationItem>
+    <NavigationItem link='/'>Checkout</NavigationItem>
   </ul>
 );
 
