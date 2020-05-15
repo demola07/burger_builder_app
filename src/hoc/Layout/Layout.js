dd1e07413bc87ca0e210454d94309f70ab367139
demola07 <45898468+demola07@users.noreply.github.com>
2020-05-15 15:23:27 +0100

    change project folder structure

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import Aux from '../../hoc/Auxillary';
-import Toolbar from '../Navigation/Toolbar/Toolbar';
-import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+import Aux from '../Auxillary/Auxillary';
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
 
 import classes from './Layout.module.css';
 
