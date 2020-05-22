a62802182d5e89793c9015ac81b438292887052c
demola07 <45898468+demola07@users.noreply.github.com>
2020-05-22 12:32:07 +0100

    fix the order button

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -11,7 +11,6 @@ import * as actionTypes from '../../store/actions';
 
 class BurgerBuilder extends Component {
   state = {
-    purchasable: false,
     purchasing: false,
     loading: false,
   };
@@ -24,9 +23,8 @@ class BurgerBuilder extends Component {
       .reduce((sum, el) => {
         return sum + el;
       }, 0);
-    this.setState({
-      purchasable: sum > 0,
-    });
+
+    return sum > 0;
   }
 
   purchaseHandler = () => {
@@ -84,7 +82,7 @@ class BurgerBuilder extends Component {
           ingredientRemoved={this.props.onIngredientRemoved}
           disabled={disableInfo}
           price={this.props.price}
-          purchasable={this.state.purchasable}
+          purchasable={this.updatePurchaseState(this.props.ings)}
           ordered={this.purchaseHandler}
         />
       </Aux>
