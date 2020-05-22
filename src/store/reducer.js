97152a7df3b9e339d64414a4b3d958f6fbd7dca3
demola07 <45898468+demola07@users.noreply.github.com>
2020-05-22 10:24:12 +0100

    implement reducer for ingredients

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -10,6 +10,28 @@ const initialState = {
   totalPrice: 4,
 };
 
-const reducer = (state = initialState, action) => {};
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.ADD_INGREDIENT:
+      return {
+        ...state,
+        ingredients: {
+          ...state.ingredients,
+          [action.ingredientName]: state.ingredients[action.ingredientName] + 1,
+        },
+      };
+    case actionTypes.REMOVE_INGREDIENT:
+      return {
+        ...state,
+        ingredients: {
+          ...state.ingredients,
+          [action.ingredientName]: state.ingredients[action.ingredientName] - 1,
+        },
+      };
+
+    default:
+      return state;
+  }
+};
 
 export default reducer;
