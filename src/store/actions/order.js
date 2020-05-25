681fd7b5aae8714f058bc92c1f8279154a8c0346
demola07 <45898468+demola07@users.noreply.github.com>
2020-05-25 23:47:50 +0100

    components cleanup

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -28,7 +28,6 @@ export const purchaseBurger = (orderData) => {
     axios
       .post('/orders.json', orderData)
       .then((response) => {
-        console.log(response);
         dispatch(purchaseBurgerSuccess(response.data.name, orderData));
       })
       .catch((err) => {
